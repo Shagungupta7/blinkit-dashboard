967c1ea93bb09904b21ad52ec911117058dadc7d
Shagun Gupta <shagungupta0307@gmail.com>
2026-03-18 10:21:49 +0530

    cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
-import KPI from './components/shared/KPI'
-import OrderPerDay from './components/OrdersPerDay'
-import { getAvgOrderValue, getCancellationRate, getTotalOrders } from './services/api'
-import DeliveryTimePerHour from './components/DeliverTimePerHour'
-import OrdersPerCity from './components/OrdersPerCity'
-import RevenuePerCity from './components/RevenuePercity'
-import AvgDeliveryTimePerCity from './components/AvgDeliveryTimePerCity'
-import CancellationPerCity from './components/CancellationPerCity'
-import Top5Riders from './components/Top5Riders'
-import OrdersPerHour from './components/OrdersPerHour'
 import Dashboard from './pages/Dashboard'
-import OrdersPerCategory from './components/OrdersPerCategory'
 
 function App() {
 
   return (
     <div className='w-full h-auto flex flex-col p-10 gap-4'>
       <Dashboard />
-      {/* <KPI title={"Total Orders"} apifunction={getTotalOrders()}/>
-      <KPI title={"Average Order Value"} apifunction={getAvgOrderValue()} />
-      <KPI title={"Cancellation Rate"} apifunction={getCancellationRate()} />
-      <OrderPerDay />
-      <OrdersPerHour />
-      <DeliveryTimePerHour />
-      <OrdersPerCity />
-      <RevenuePerCity />
-      <AvgDeliveryTimePerCity />
-      <CancellationPerCity />
-      <Top5Riders /> */}
     </div>
   )
 }
